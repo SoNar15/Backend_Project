d8305f7f07794e1a251a891368253219edf6a737
SoNar15 <sohamnarkhedkar@gmail.com>
2024-10-12 11:05:43 +0530

    setup project files part 1

---
+++ src/notes.js
@@ -0,0 +1,11 @@
+//  touch app.js constants.js index.js  to create files in folder 
+// Only for bash command line
+
+// Git does not track folders , it tracks files
+// So if you want to keep the track of the folder use =>  .gitkeep
+
+// Nodemon
+// Starts the server as soon as the files are saved.
+// Reduces manual start , stop of the server.
+// Dev Dependency :=
+// Does not affect deployment
